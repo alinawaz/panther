3ec41948153a0408ae69a7e7699bfa161bb50f5d
Ali Nawaz <alinawazsolid@gmail.com>
2018-10-02 01:50:18 +0500

    Request improvements

--- panther/http/Request.php
+++ panther/http/Request.php
@@ -2,8 +2,16 @@
 
 namespace Panther\Http;
 
-class Request {
+class Request implements RequestInterface {
 	
-	
+	private $variables = [];
+
+	public function get($name){
+		return $this->variables[$name];
+	}
+
+	public function set($name, $value){
+		$this->variables[$name] = $value;
+	}
 
 }
---
+++ panther/http/RequestInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Panther\Http;
+
+interface RequestInterface {
+	
+	public function get($name);
+
+	public function set($name, $value);
+
+}
