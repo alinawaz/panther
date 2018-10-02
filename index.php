43d3a9615e3dd20ce0e4e71a077745317eaf13d1
Ali Nawaz <ali.nawaz@zintechnologies.com>
2018-10-02 17:50:23 +0500

    index.php comments

--- index.php
+++ index.php
@@ -2,12 +2,26 @@
 
 require 'vendor/autoload.php';
 
+/* Global helper file */
 include_once 'panther/helper.php';
 
+/*
+    New application initialization
+    Constructor takes in configs
+    base_url is used for no virtual server environment
+*/ 
 $app = new \Panther\App(['base_url' => 'http://localhost:8080/panther']);
 
+/*
+    Entities registration
+    All entity classes needs to be registered here
+    Panther will take care of migrations, seeders, routes & models for you!
+*/
 $app->register([
     'class' => App\Entities\HelloEntity::class
 ]);
 
+/*
+    Running the application
+*/
 $app->run();
