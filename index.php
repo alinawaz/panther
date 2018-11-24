c65d8d4310f6381939419483ac7ffee131931b57
Ali Nawaz <alinawazsolid@gmail.com>
2018-11-25 02:33:05 +0500

    removed database neityt

--- index.php
+++ index.php
@@ -17,8 +17,5 @@ $app->register('HelloEntity', 'hello');
 /* Documentation Entity */
 $app->register('DocumentationEntity', 'documentation');
 
-// Database testing entity
-$app->register('DatabaseEntity', 'database');
-
 /* Running the application */
 $app->run();
